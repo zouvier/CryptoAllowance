54e40dde31509cda35225c0e6c34062f6d8a81fc
Zoumana Cisse <zoumanacisse692@gmail.com>
2021-07-10 14:31:43 -0700

    Began process for setting up owner from open zepplin

--- WeeklyAllowance.sol
+++ WeeklyAllowance.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
+import '/Users/zouvier/projects/openzeppelin-contracts/contracts/access/Ownable.sol';
+
 
 //Made by ZOUVIER! 
 // chose 0.8.0 because of the already implemented safe math, Also, i left some of the values as public for easy testing on Remix IDE. 
@@ -8,24 +10,13 @@
 
 pragma solidity ^0.8.0;
 
-contract SharedWallet {
+contract SharedWallet is Ownable{
     
     event Received(address, uint);
     
     // address of the owner, made it private so that it can only be accessed from within this contract.
-    address private owner;
+    //address private owner;
     
-    // initialized owner variable.
-    constructor()  payable{
-        owner = msg.sender;
-    }
-    
-    
-    // created modifier for chekcing if address is the owner of the contract. 
-    modifier IsOwner {
-        require(msg.sender == owner, "Contact the owner of this Contract for assitance");
-        _;
-    }
     //check to see if address is able to withdraw weeklt allowance.
     modifier CheckCred {
         require(Trustee[msg.sender].can_withdraw == true && Trustee[msg.sender].timeUntilNextWithdraw < (block.timestamp), "Sorry you're either too early or no longer have access to this wallet");
@@ -48,7 +39,7 @@ contract SharedWallet {
     
     // function to initialize an address to the shared wallet. can only be done by the owner of the wallet. 
     // MAKE PRIVATE
-    function addToTrustedListAndPay(address _trustedAddress, uint weeklyAmount) IsOwner external  {
+    function addToTrustedListAndPay(address _trustedAddress, uint weeklyAmount) onlyOwner external  {
         require(Trustee[_trustedAddress].alreadySet == false);
         Trustee[_trustedAddress].alreadySet = true;
         Trustee[_trustedAddress].timeUntilNextWithdraw = block.timestamp + 1 weeks;
@@ -60,14 +51,14 @@ contract SharedWallet {
     
     //allow the owner of the contract to remove an address from the trusted list.
     // MAKE PRIVATE
-    function removeFromTrustedList(address _trustedAddress) IsOwner external {
+    function removeFromTrustedList(address _trustedAddress) onlyOwner external {
         require(Trustee[_trustedAddress].can_withdraw = false);
     }
     
     
     // allows an address to withdraw allowances again. adding them to the trusted list. 
     // MAKE PRIVATE
-    function addAgain(address _trustedAddress) IsOwner external {
+    function addAgain(address _trustedAddress) onlyOwner external {
         require(Trustee[_trustedAddress].alreadySet == true);
         Trustee[_trustedAddress].timeUntilNextWithdraw = block.timestamp + 1 weeks;
         Trustee[_trustedAddress].can_withdraw = true;
@@ -83,14 +74,14 @@ contract SharedWallet {
     
     //allows the owner to withdraw any amount they want
     // MAKE PRIVATE
-    function ownerWithdraws(uint amount) IsOwner external payable {
-        payable(owner).transfer(amount);
+    function ownerWithdraws(uint amount) onlyOwner external payable {
+        payable(_owner).transfer(amount);
     }
     
     
     // allows the owner to check the value of the contract
     //MAKE PRIVATE
-    function CheckAddressValue() view external IsOwner returns(uint) {
+    function CheckAddressValue() view external onlyOwner returns(uint) {
         return ((address(this).balance)/1 ether);
     }
     
